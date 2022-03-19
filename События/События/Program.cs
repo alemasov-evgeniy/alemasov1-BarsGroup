f0320e423c3bb5ef1a796d4a52e8871efd0e1ec6
e.alemasov <alemasov.evgeniy@mail.ru>
2022-03-19 16:52:32 +0300

    Обновлены файлы программы События

--- События/События/Program.cs
+++ События/События/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace События
 {
@@ -10,6 +6,33 @@ namespace События
     {
         static void Main(string[] args)
         {
+            Symbol symbol = new Symbol();
+            symbol.OnKeyPressed += PrintSymbol;
+            symbol.Run();
+            Console.ReadKey();
         }
+
+        private static void PrintSymbol(object sender, char e)
+        {
+            Console.WriteLine($"\nВведен символ {e}");
+        }
+    }
+
+    public class Symbol
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public void Run()
+        {
+            Console.Write("Введите символ: ");
+            char s = Console.ReadKey().KeyChar;
+            while (s != 'c' & s != 'C')
+            {
+                OnKeyPressed?.Invoke(this, s);
+                Console.Write("\nВведите символ: ");
+                s = Console.ReadKey().KeyChar;
+            }
+            Console.WriteLine("\nЗавершение работы программы.");
+        }
+         
     }
 }
